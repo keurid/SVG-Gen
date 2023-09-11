1a0f4c37488c7f0cf2ebd6e5edd9fa747d310873
keurid <kevindesantiago99@gmail.com>
2023-09-10 22:22:23 -0700

    finished index.js

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
+
+async function generateLogo() {
+  const userInput = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters for the text:',
+      validate: (input) => input.length <= 3,
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter the text color (color keyword or hexadecimal):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter the shape color (color keyword or hexadecimal):',
+    },
+  ]);
+
+  let shape;
+  switch (userInput.shape) {
+    case 'circle':
+      shape = new Circle();
+      break;
+    case 'triangle':
+      shape = new Triangle();
+      break;
+    case 'square':
+      shape = new Square();
+      break;
+  }
+
+  const textColor = userInput.textColor;
+  const shapeColor = userInput.shapeColor;
+  shape.setColor(shapeColor);
+
+  const svgString = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${shape.render()}
+      <text x="50%" y="50%" font-size="48" text-anchor="middle" fill="${textColor}">
+        ${userInput.text}
+      </text>
+    </svg>
+  `;
+
+  fs.writeFileSync('logo.svg', svgString);
+
+  console.log('Generated logo.svg');
+}
+
+generateLogo();
